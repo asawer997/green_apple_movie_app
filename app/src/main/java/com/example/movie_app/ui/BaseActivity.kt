97b1aa27abe1fc4c901380a813070bb41becfec2
HIPPODATA\ahs <ahs@hippodata.net>
2021-09-14 09:59:35 +0200

    Code refactor, adding Json Data and Activities instead of Fragments.

---
+++ app/src/main/java/com/example/movie_app/ui/BaseActivity.kt
@@ -0,0 +1,35 @@
+package com.example.movie_app.ui
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
+    //initial variable for log fun
+    abstract val LOG_TAG: String
+
+    //initial binding inflater
+    abstract val bindingInflater: (LayoutInflater) -> VB
+    private var _binding: ViewBinding? = null
+    protected val binding
+        get() = _binding as VB?
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = bindingInflater(layoutInflater)
+        setContentView(requireNotNull(_binding).root)
+        setup()
+        addCallbacks()
+    }
+    //basic function in this project
+    protected abstract fun setup()
+    protected abstract fun addCallbacks()
+    protected fun log(value: Any) {
+        Log.v(LOG_TAG, value.toString())
+    }
+
+
+
+}
